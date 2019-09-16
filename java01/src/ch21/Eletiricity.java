708c4abc97ba4894fed6bc6360e354cff575d8c1
leewoo <dlguddn3214@naver.com>
2019-09-16 20:32:56 +0900

    no message

---
+++ java01/src/ch21/Eletiricity.java
@@ -0,0 +1,9 @@
+package ch21;
+
+public class Eletiricity {
+	
+	public static void main(String[] args) {
+		Machine m1 = new Machine();
+		m1.run();
+	}
+}
---
+++ java01/src/ch21/Machine.java
@@ -0,0 +1,11 @@
+package ch21;
+
+public class Machine {
+
+	public void run() {
+		System.out.println("running...");
+	}
+	
+
+
+}
