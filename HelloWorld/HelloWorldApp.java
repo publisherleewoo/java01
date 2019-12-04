2e4d51752813c2574321e0c5bf02920de0e4950b
leewoo <dlguddn3214@naver.com>
2019-12-05 03:47:29 +0900

    no message

---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+ 
+public class HelloWorldApp {
+	public static void main(String[] args) {
+		 String id = JOptionPane.showInputDialog("Enter a ID");
+		Scanner sn = new Scanner(System.in);	
+		String name;
+		String fname;
+		System.out.println("당신의 이름은?");
+		name = sn.next();
+		System.out.println("당신의 성은?");
+		fname = sn.next();
+		System.out.println(id + "" + name + " " + fname + "님 안녕하세요");
+	}
+}
