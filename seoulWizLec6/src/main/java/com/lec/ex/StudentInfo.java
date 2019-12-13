9656a68ab017e501df8e1cf5ec3a6405d1ebfa8c
leewoo <dlguddn3214@naver.com>
2019-12-13 14:20:03 +0900

    no message

---
+++ seoulWizLec6/src/main/java/com/lec/ex/StudentInfo.java
@@ -0,0 +1,20 @@
+package com.lec.ex;
+
+public class StudentInfo {
+	private Student student;
+
+//	public StudentInfo(Student student) {
+//	
+//	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
+	
+	
+}
