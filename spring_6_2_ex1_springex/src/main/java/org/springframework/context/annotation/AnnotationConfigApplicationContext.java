28d08c7ff37b6af92e3018c3d649f36f460de345
leewoo <dlguddn3214@naver.com>
2019-12-14 14:12:40 +0900

    no message

---
+++ spring_6_2_ex1_springex/src/main/java/org/springframework/context/annotation/AnnotationConfigApplicationContext.java
@@ -0,0 +1,11 @@
+package org.springframework.context.annotation;
+
+import com.javalec.ex.ApplicationConfig;
+
+public class AnnotationConfigApplicationContext {
+
+	public AnnotationConfigApplicationContext(Class<ApplicationConfig> class1) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
